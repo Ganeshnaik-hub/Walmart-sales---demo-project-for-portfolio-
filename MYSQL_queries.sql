5ff07a2dff455147d494c31644c991104de83cfd
Ganeshnaik-hub <90789447+Ganeshnaik-hub@users.noreply.github.com>
2023-11-16 17:43:32 +0530

    Update MYSQL_queries.sql

--- MYSQL_queries.sql
+++ MYSQL_queries.sql
@@ -1,9 +1,6 @@
--- Create database
-CREATE DATABASE IF NOT EXISTS walmartSales;
+create database walmartsales;
 
-
--- Create table
-CREATE TABLE IF NOT EXISTS sales(
+create table sales (
 	invoice_id VARCHAR(30) NOT NULL PRIMARY KEY,
     branch VARCHAR(5) NOT NULL,
     city VARCHAR(30) NOT NULL,
@@ -12,24 +9,20 @@ CREATE TABLE IF NOT EXISTS sales(
     product_line VARCHAR(100) NOT NULL,
     unit_price DECIMAL(10,2) NOT NULL,
     quantity INT NOT NULL,
-    tax_pct FLOAT(6,4) NOT NULL,
+    tax_pct DECIMAL(6,4),
     total DECIMAL(12, 4) NOT NULL,
     date DATETIME NOT NULL,
     time TIME NOT NULL,
     payment VARCHAR(15) NOT NULL,
     cogs DECIMAL(10,2) NOT NULL,
-    gross_margin_pct FLOAT(11,9),
+    gross_margin_pct DECIMAL(11,9),
     gross_income DECIMAL(12, 4),
-    rating FLOAT(2, 1)
-);
-
--- Data cleaning
-SELECT
-	*
-FROM sales;
-
-
--- Add the time_of_day column
+    rating DECIMAL(2, 1)
+    );
+    
+    select * from sales;
+    
+------- Add the time_of_day column
 SELECT
 	time,
 	(CASE
@@ -38,13 +31,15 @@ SELECT
         ELSE "Evening"
     END) AS time_of_day
 FROM sales;
-
-
 ALTER TABLE sales ADD COLUMN time_of_day VARCHAR(20);
 
 -- For this to work turn off safe mode for update
 -- Edit > Preferences > SQL Edito > scroll down and toggle safe mode
--- Reconnect to MySQL: Query > Reconnect to server
+-- Reconnect to MySQL: Query > Reconnect to server 
+------   or we can use below syntax ----
+SET SQL_SAFE_UPDATES = 0;
+--------------------------------
+
 UPDATE sales
 SET time_of_day = (
 	CASE
@@ -54,8 +49,8 @@ SET time_of_day = (
     END
 );
 
-
--- Add day_name column
+---------------------------------------------------------
+--------------------------- Add day_name column
 SELECT
 	date,
 	DAYNAME(date)
@@ -66,8 +61,8 @@ ALTER TABLE sales ADD COLUMN day_name VARCHAR(10);
 UPDATE sales
 SET day_name = DAYNAME(date);
 
-
--- Add month_name column
+---------------------------------------------------------
+--------------------- Add month_name column
 SELECT
 	date,
 	MONTHNAME(date)
@@ -79,24 +74,23 @@ UPDATE sales
 SET month_name = MONTHNAME(date);
 
 -- --------------------------------------------------------------------
--- ---------------------------- Generic ------------------------------
+-- -------- Generic questions and answeres ------------------------------
 -- --------------------------------------------------------------------
--- How many unique cities does the data have?
+--------- How many unique cities does the data have?
 SELECT 
 	DISTINCT city
 FROM sales;
 
--- In which city is each branch?
+-------- In which city is each branch?
 SELECT 
 	DISTINCT city,
     branch
 FROM sales;
 
 -- --------------------------------------------------------------------
--- ---------------------------- Product -------------------------------
+-- -------- Product based questions and answers -------------------------------
 -- --------------------------------------------------------------------
-
--- How many unique product lines does the data have?
+--------- How many unique product lines does the data have?
 SELECT
 	DISTINCT product_line
 FROM sales;
@@ -110,7 +104,7 @@ FROM sales
 GROUP BY product_line
 ORDER BY qty DESC;
 
--- What is the most selling product line
+------     What is the most selling product line
 SELECT
 	SUM(quantity) as qty,
     product_line
@@ -118,7 +112,7 @@ FROM sales
 GROUP BY product_line
 ORDER BY qty DESC;
 
--- What is the total revenue by month
+------ What is the total revenue by month
 SELECT
 	month_name AS month,
 	SUM(total) AS total_revenue
@@ -127,7 +121,7 @@ GROUP BY month_name
 ORDER BY total_revenue;
 
 
--- What month had the largest COGS?
+-------- What month had the largest COGS?
 SELECT
 	month_name AS month,
 	SUM(cogs) AS cogs
@@ -136,7 +130,7 @@ GROUP BY month_name
 ORDER BY cogs;
 
 
--- What product line had the largest revenue?
+------ What product line had the largest revenue?
 SELECT
 	product_line,
 	SUM(total) as total_revenue
@@ -154,7 +148,7 @@ GROUP BY city, branch
 ORDER BY total_revenue;
 
 
--- What product line had the largest VAT?
+----  What product line had the largest VAT?
 SELECT
 	product_line,
 	AVG(tax_pct) as avg_tax
@@ -162,9 +156,8 @@ FROM sales
 GROUP BY product_line
 ORDER BY avg_tax DESC;
 
-
--- Fetch each product line and add a column to those product 
--- line showing "Good", "Bad". Good if its greater than average sales
+--------- Fetch each product line and add a column to those product 
+--------- line showing "Good", "Bad". Good if its greater than average sales
 
 SELECT 
 	AVG(quantity) AS avg_qnty
@@ -180,7 +173,7 @@ FROM sales
 GROUP BY product_line;
 
 
--- Which branch sold more products than average product sold?
+-------- Which branch sold more products than average product sold?
 SELECT 
 	branch, 
     SUM(quantity) AS qnty
@@ -189,7 +182,7 @@ GROUP BY branch
 HAVING SUM(quantity) > (SELECT AVG(quantity) FROM sales);
 
 
--- What is the most common product line by gender
+-------- What is the most common product line by gender
 SELECT
 	gender,
     product_line,
@@ -198,7 +191,7 @@ FROM sales
 GROUP BY gender, product_line
 ORDER BY total_cnt DESC;
 
--- What is the average rating of each product line
+-------- What is the average rating of each product line
 SELECT
 	ROUND(AVG(rating), 2) as avg_rating,
     product_line
@@ -210,21 +203,21 @@ ORDER BY avg_rating DESC;
 -- --------------------------------------------------------------------
 
 -- --------------------------------------------------------------------
--- -------------------------- Customers -------------------------------
+-- ----------- Customers based questions -------------------------------
 -- --------------------------------------------------------------------
 
--- How many unique customer types does the data have?
+-------- How many unique customer types does the data have?
 SELECT
 	DISTINCT customer_type
 FROM sales;
 
--- How many unique payment methods does the data have?
+-------- How many unique payment methods does the data have?
 SELECT
 	DISTINCT payment
 FROM sales;
 
 
--- What is the most common customer type?
+--------- What is the most common customer type?
 SELECT
 	customer_type,
 	count(*) as count
@@ -232,7 +225,7 @@ FROM sales
 GROUP BY customer_type
 ORDER BY count DESC;
 
--- Which customer type buys the most?
+--------- Which customer type buys the most?
 SELECT
 	customer_type,
     COUNT(*)
@@ -240,7 +233,7 @@ FROM sales
 GROUP BY customer_type;
 
 
--- What is the gender of most of the customers?
+---------- What is the gender of most of the customers?
 SELECT
 	gender,
 	COUNT(*) as gender_cnt
@@ -248,7 +241,7 @@ FROM sales
 GROUP BY gender
 ORDER BY gender_cnt DESC;
 
--- What is the gender distribution per branch?
+--------- What is the gender distribution per branch?
 SELECT
 	gender,
 	COUNT(*) as gender_cnt
@@ -256,19 +249,14 @@ FROM sales
 WHERE branch = "C"
 GROUP BY gender
 ORDER BY gender_cnt DESC;
--- Gender per branch is more or less the same hence, I don't think has
--- an effect of the sales per branch and other factors.
 
--- Which time of the day do customers give most ratings?
+------- Which time of the day do customers give most ratings?
 SELECT
 	time_of_day,
 	AVG(rating) AS avg_rating
 FROM sales
 GROUP BY time_of_day
 ORDER BY avg_rating DESC;
--- Looks like time of the day does not really affect the rating, its
--- more or less the same rating each time of the day.alter
-
 
 -- Which time of the day do customers give most ratings per branch?
 SELECT
@@ -290,9 +278,6 @@ FROM sales
 GROUP BY day_name 
 ORDER BY avg_rating DESC;
 -- Mon, Tue and Friday are the top best days for good ratings
--- why is that the case, how many sales are made on these days?
-
-
 
 -- Which day of the week has the best average ratings per branch?
 SELECT 
@@ -303,12 +288,11 @@ WHERE branch = "C"
 GROUP BY day_name
 ORDER BY total_sales DESC;
 
-
 -- --------------------------------------------------------------------
 -- --------------------------------------------------------------------
 
 -- --------------------------------------------------------------------
--- ---------------------------- Sales ---------------------------------
+-- ---------Sales Related Questions ---------------------------------
 -- --------------------------------------------------------------------
 
 -- Number of sales made in each time of the day per weekday 
